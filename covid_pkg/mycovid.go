cc994203a9e0aa0e27a4a71fee3bb516fa6070f9
UsmanAttiq <usmanattiq28@outlook.com>
2020-03-29 16:18:51 +0500

    initial project version

---
+++ covid_pkg/mycovid.go
@@ -0,0 +1,19 @@
+package covid
+
+var PCases,SKCases,FCases int =1526, 9583, 37575
+
+func PrintPakistanCases()int{
+	return PCases
+}
+
+func PrintSouthKoreaCases()int{
+	return SKCases
+}
+
+func PrintFranceCases()int{
+	return FCases
+}
+
+func PrintMessage()string{
+	return "Just when I was starting to actively strive for what I wanted from life. Corona Strikes. Or maybe this is an excuse, kidding. Stay safe in Quarantine!"
+}
